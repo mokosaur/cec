Adjust BIC calculation and fix multidimensional optimization

cec.py
@@ -144,7 +144,7 @@ class NormalCEC(CEC):
     def _init_params(self, clusters):
         self.cluster_centers_ = np.vstack(clusters)
         self.W = np.zeros((self.n_clusters, self.X.shape[1], self.X.shape[1]))
-        self.params = [self.W, self.cluster_centers_, np.array([self.n_clusters])]
+        self.params = [self.W, self.cluster_centers_, self.p[:-1]]
         for k in range(self.n_clusters):
             if self.X[self.y == k, :].shape[0] != 0:
                 self.cluster_centers_[k] = self.X[self.y == k, :].mean(axis=0)
@@ -170,7 +170,7 @@ class SGCEC(CEC):
         self.W = np.zeros((self.n_clusters, self.X.shape[1], self.X.shape[1]))
         self.sigma = np.zeros((self.n_clusters, self.X.shape[1]))
         self.tau = np.zeros((self.n_clusters, self.X.shape[1]))
-        self.params = [self.W, self.cluster_centers_, np.array([self.n_clusters]), self.sigma, self.tau]
+        self.params = [self.W, self.cluster_centers_, self.p[:-1], self.sigma]
         for k in range(self.n_clusters):
             if self.X[self.y == k, :].shape[0] != 0:
                 self.cluster_centers_[k] = self.X[self.y == k, :].mean(axis=0)
@@ -204,7 +204,7 @@ class GammaCEC(CEC):
         self.sigma = np.zeros((self.n_clusters, self.X.shape[1]))
         self.tau = np.zeros((self.n_clusters, self.X.shape[1]))
         self.c = np.full(self.n_clusters, 2.)
-        self.params = [self.W, self.cluster_centers_, self.c, np.array([self.n_clusters]), self.sigma, self.tau]
+        self.params = [self.W, self.cluster_centers_, self.c, self.p[:-1], self.sigma]
         for k in range(self.n_clusters):
             if self.X[self.y == k, :].shape[0] != 0:
                 self.cluster_centers_[k] = self.X[self.y == k, :].mean(axis=0)

gradients.py
@@ -143,7 +143,6 @@ def optimize_split_gamma(X, m, W, c, method="L-BFGS-B", jac=True):
     c0 = c
     x0 = np.append(np.vstack([m0, W0]).flatten(), c0)
     if jac:
-        #         jac = (lambda x: gradient_split_gamma(X, x[:-1].reshape((3, 2))[0, :], x[:-1].reshape((3, 2))[1:, :], x[-1]))
         jac = (lambda x: -gradient_split_gamma(X, x[:-1].reshape((d + 1, d))[0, :], x[:-1].reshape((d + 1, d))[1:, :], x[-1]))
     # params = minimize(lambda x: np.log(l_function_gamma(X, x[-1].reshape((3, 2))[0, :],
     #                                x[:-1].reshape((3, 2))[1:, :], x[-1])),
@@ -154,9 +153,9 @@ def optimize_split_gamma(X, m, W, c, method="L-BFGS-B", jac=True):
                                                       x[:-1].reshape((d + 1, d))[1:, :], x[-1]),
                       x0,
                       jac=jac, method=method,
-                      bounds=(
-                          (None, None), (None, None), (None, None), (None, None), (None, None), (None, None),
-                          (0.001, 10)))
+                      bounds=tuple((None, None) if x < len(x0) - 1 else (0.001, 10) for x in range(len(x0))))
+                          # (None, None), (None, None), (None, None), (None, None), (None, None), (None, None),
+                          # (0.001, 10)))
     m0 = params.x[:-1].reshape((d + 1, d))[0, :]
     W0 = params.x[:-1].reshape((d + 1, d))[1:, :]
     c0 = params.x[-1]